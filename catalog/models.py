4e3eda808887061db1d362a71776707244243967
Roman Sokolov <gnonasis@gmail.com>
2024-11-24 13:50:05 +0200

    add constrain at least one field filled of comment and rating must be filled model Feedback

--- catalog/models.py
+++ catalog/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Avg
+from django.db.models import Q
+
 from my_precious.settings import AUTH_USER_MODEL
 
 
